4f62230473b57b9ab365d3540f5df68212c26923
Ryan Kelly <ryan@kiiii.com>
2024-08-14 15:59:08 +1000

    Init SVAR example

---
+++ npe_convergence/scripts/run_svar_experiments.py
@@ -0,0 +1,10 @@
+from npe_convergence.scripts.run_experiment import run_experiment
+from npe_convergence.scripts.run_svar import run_svar
+
+def run_svar_experiments():
+    kl_mat = run_experiment(run_svar)
+    return None
+
+
+if __name__ == "__main__":
+    run_svar_experiments()
