160fc84953f83aa91597bfb2868ef6fe9896a353
Ryan Kelly <ryan@kiiii.com>
2024-08-19 17:27:43 +1000

    Update MA(2) for exact sampling

--- npe_convergence/examples/ma2.py
+++ npe_convergence/examples/ma2.py
@@ -5,6 +5,33 @@ import jax.random as random
 import numpyro
 from numpyro import distributions as dist
 
+
+def autocov_exact(thetas, k, ma_order=1):
+    # NOTE: ASSUMPTION - white noise has constant variance one
+    res = 0
+    # NOTE: assume passing in thetas care about, add in 1 for i=0 case
+    thetas = jnp.concatenate((jnp.array([1.0]), thetas))
+    for i in range(ma_order + 1):
+        if i - k >= 0:
+            res += thetas[i] * thetas[i - k]
+    return res
+
+
+def sample_autocov_variance(thetas, k, n_obs, ma_order=1):
+    res = 0
+    for i in range(-ma_order, ma_order + 1):
+        tmp_k = i if i >= 0 else -i
+        res += autocov_exact(thetas, tmp_k, ma_order) ** 2
+        res = jnp.where(
+            jnp.abs(k + i) <= ma_order,
+            res + autocov_exact(thetas, k + i, ma_order) * autocov_exact(thetas, k - i, ma_order),
+            res
+        )
+
+    res = res / n_obs
+
+    return res
+
 def MA2(t1, t2, n_obs=100, batch_size=1, key=None):
     # TODO: could make faster (e.g. use scan)
     # NOTE: adapted from ELFI
@@ -79,28 +106,28 @@ class CustomPrior_t2:
         return (x >= locs) & (x <= locs + scales) * (1 / jnp.where(scales > 0, scales, 1))
 
 
-def numpyro_model(var_obs, autocov_obs1, autocov_obs2, a=2, n_obs=100, w_key=None):
+def numpyro_model(obs, a=2, n_obs=100):
     t1 = numpyro.sample('t1', dist.Uniform(-a, a))
     locs = jnp.maximum(-a - t1, -a + t1)
     scales = a - locs
     t2 = numpyro.sample('t2', dist.Uniform(locs, locs + scales))
 
-    n_key = numpyro.prng_key()
-    sub_key = n_key if n_key is not None else w_key
+    # n_key = numpyro.prng_key()
+    # sub_key = n_key if n_key is not None else w_key
 
-    w = random.normal(sub_key, (n_obs + 2,))
+    # w = random.normal(sub_key, (n_obs + 2,))
     # w = numpyro.sample('w', dist.Normal(0, 1).expand([n_obs + 2]), obs=None)
-    x = w[2:] + t1 * w[1:-1] + t2 * w[:-2]
+    # x = w[2:] + t1 * w[1:-1] + t2 * w[:-2]
 
-    var = jnp.var(x)
-    autocov1 = jnp.mean(x[1:] * x[:-1])  # TODO: confirm (could use numpyro.deterministic)
-    autocov2 = jnp.mean(x[2:] * x[:-2])
-    numpyro.deterministic('autocov', jnp.array([autocov1, autocov2]))
+    # var = jnp.var(x)
+    # autocov1 = jnp.mean(x[1:] * x[:-1])  # TODO: confirm (could use numpyro.deterministic)
+    # autocov2 = jnp.mean(x[2:] * x[:-2])
+    # numpyro.deterministic('autocov', jnp.array([autocov1, autocov2]))
 
     # TODO!!! HOW SET NORMAL VAR FOR EXACT? DOES IT MATTER?
-    variance_scale = 1 / jnp.sqrt(n_obs)  # TODO: CLT / SIMILAR JUSTIFICATION?
+    # variance_scale = 1 / jnp.sqrt(n_obs)  # TODO: CLT / SIMILAR JUSTIFICATION?
+    thetas = jnp.array([t1, t2])  # TODO: MAKE GENERAL
+    y_variance = [sample_autocov_variance(thetas, k, n_obs, len(thetas)) for k in range(len(obs))]
 
-    numpyro.sample('obs_var', dist.Normal(var, variance_scale), obs=var_obs)  # TODO: HALF NORMAL OR SOMETHING?
-    numpyro.sample('obs_autocov1', dist.Normal(autocov1, variance_scale), obs=autocov_obs1)
-    numpyro.sample('obs_autocov2', dist.Normal(autocov2, variance_scale), obs=autocov_obs2)
-    return x
+    for i in range(len(y_variance)):
+        numpyro.sample(f'obs_{i}', dist.Normal(autocov_exact(thetas, i, len(thetas)), jnp.sqrt(y_variance[i])), obs=obs[i])
